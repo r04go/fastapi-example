c02eec4398a281ab50ce4ef231095ebe46396305
Roy <102919843+r04go@users.noreply.github.com>
2022-04-04 12:01:37 -0500

    Update database.py

--- config/database.py
+++ config/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine('mysql+pymysql://kamajuhaq5a11lei:g8a4m4xzq8cd4uu5@dcrhg4kh56j13bnu.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/iotbi0bkia5eryb7')
+engine = create_engine('mysql+pymysql://')
 
 meta = MetaData()
 
